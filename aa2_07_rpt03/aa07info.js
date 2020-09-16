1756ea69a28896c760a16d35ca67b4b77b1af7d7
손성빈 <48310093+ijseongbin@users.noreply.github.com>
2020-09-16 17:43:44 +0900

    Add files via upload

---
+++ aa2_07_rpt03/aa07info.js
@@ -0,0 +1,6 @@
+module.exports = function (id, name, phone) {
+    console.log("my info");
+    console.log("Id : " + id);
+    console.log("name : " + name);
+    console.log("phone : " + phone + "\n");
+};
---
+++ aa2_07_rpt03/index2.js
@@ -0,0 +1,16 @@
+//http server : index.js
+var http = require ("http");
+port = 2000;
+
+var server = http.createServer((request, response ) => {
+    response.writeHeader(200, {
+        "Content-Type": "text/plain",
+    });
+    response.write("Hello HTTP server frome node.js"); //WEB response
+    response.write("\n");
+    response.write("My ID is aa2-07! \n");
+    response.end();
+});
+
+server.listen(port);
+console.log("Server Running on " + port + ".\nLaunch http://localhost:" + port)
